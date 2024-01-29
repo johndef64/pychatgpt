b7f6f552643c0ad8a3cc8e0faf04f6d21164770c
johndef64 <116732521+johndef64@users.noreply.github.com>
2024-01-29 11:52:46 +0100

    Update auto_gpt.py

--- auto-gpt/auto_gpt.py
+++ auto-gpt/auto_gpt.py
@@ -1,7 +1,28 @@
 import os
 import time
-import importlib
 from datetime import datetime
+import requests
+
+
+# check requirements
+def get_gitfile(url, flag='', dir = os.getcwd()):
+    url = url.replace('blob','raw')
+    response = requests.get(url)
+    file_name = flag + url.rsplit('/',1)[1]
+    file_path = os.path.join(dir, file_name)
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+
+if not os.getcwd().endswith('pychatgpt.py'):
+    handle="https://raw.githubusercontent.com/johndef64/pychatgpt/main/"
+    files = ["pychatgpt.py"]
+    for file in files:
+        url = handle+file
+        get_gitfile(url)
+
+import pychatgpt as op
+op.check_and_install_module('pyperclip')
+import pyperclip
 
 def get_boolean_input(prompt):
     while True:
@@ -10,31 +31,8 @@ def get_boolean_input(prompt):
         except KeyError:
             print("Invalid input. Please enter 'y' or 'n'.")
 
-def check_and_install_module(module_name):
-    try:
-        # Check if the module is already installed
-        importlib.import_module(module_name)
-        #import module_name
-        #print(f"The module '{module_name}' is already installed.")
-    except ImportError:
-        # If the module is not installed, try installing it
-        x = get_boolean_input(
-            "\n" + module_name + "  module is not installed.\nwould you like to install it? (y/n):")
-        if x:
-            import subprocess
-            subprocess.check_call(["pip", "install", module_name])
-            print(f"The module '{module_name}' was installed correctly.")
-        else:
-            exit()
 
-# check requirements
-try:
-    import pyperclip
-    import openai
-except ImportError:
-    print('Check requirements:')
-check_and_install_module("pyperclip")
-check_and_install_module("openai")
+
 #print('--------------------------------------')
 
 current_dir = ''
@@ -45,38 +43,17 @@ if os.getcwd() == 'C:\WINDOWS\System32':
 else:
     current_dir = os.getcwd()
 
-
-# If  import openai
-import openai
-import pyperclip
-
 # check Usage:
 # https://platform.openai.com/account/usage
 
 # Set API key:
 # https://platform.openai.com/account/api-keys
-api_key = None
-if not os.path.isfile(current_dir + '/openai_api_key.txt'):
-    with open(current_dir + '/openai_api_key.txt', 'w') as file:
-        file.write(input('insert here your openai api key:'))
 
-api_key = open(current_dir + '/openai_api_key.txt', 'r').read()
-openai.api_key = str(api_key)
-
-
-# Log Inizialization:
-if not os.path.isfile(current_dir + '/conversation_log.txt'):
-    with open(current_dir + '/conversation_log.txt', 'w') as file:
-        file.write('Auto-GPT\n\nConversation LOG:\n')
-        print(str('\nconversation_log.txt created at ' + os.getcwd()))
 
 # Parameters--------------------------
-total_tokens = 0
-maxtoken = 1000
-prmtoken = 8100 - (maxtoken*1.3)
-keep_persona = True
 persona = ''
-model = 'gpt-4'
+#model = 'gpt-4'
+
 add = "You are a helpful assistant."
 
 '''InvalidRequestError: This model's maximum context length is 4097 tokens. However, you requested 4201 tokens (3201 in the messages, 1000 in the completion). Please reduce the length of the messages or completion.
@@ -124,113 +101,6 @@ dictita = {
 language = ''
 dict = dicteng
 
-
-# Chat Functions ----------------------------
-#https://platform.openai.com/account/rate-limits
-def ask_gpt(prompt):
-    completion = openai.ChatCompletion.create(
-        model= model,
-        messages=[
-            {"role": "system", "content": add},
-            {"role": "user", "content": prompt}
-        ])
-
-    with open('conversation_log.txt', 'a', encoding='utf-8') as file:
-        file.write('---------------------------')
-        if add != '':
-            file.write('\nSystem: \n"' + add+'"\n')
-        file.write('\nUser: '+str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))+'\n' + prompt)
-        file.write('\n\nGPT:\n' + completion.choices[0].message.content + '\n\n')
-
-    print_mess = prompt.replace('\r', '\n').replace('\n\n', '\n')
-    print('user:',print_mess,'\n')
-    return print(completion.choices[0].message.content)
-
-# Initialize the conversation (text-davinci-003)----------------
-conversation = [
-    {"role": "system", "content": "You are a helpful assistant expert in science and informatics."},
-]
-def expand_conversation(message):
-    conversation.append({'role': 'user', 'content': message})
-def build_prompt(conversation):
-    prompt = ""
-    for message in conversation:
-        role = message['role']
-        content = message['content']
-        prompt += f'{role}: {content}\n'
-    return prompt
-def send_message(message):
-    expand_conversation(message)
-    response = openai.Completion.create(
-        engine='text-davinci-003',
-        prompt=build_prompt(conversation),
-        temperature=0.7,
-        max_tokens=2000,
-        n=1,
-        stop=None
-    )
-    conversation.append({'role': 'assistant', 'content': response.choices[0].text.strip()})
-    # Print the assistant's response
-    print(response.choices[0].text.strip())
-
-
-# Initialize the conversation (gpt-4)----------------------------
-
-# Conversation history
-conversation_gpt = []
-
-def expand_conversation_gpt(message):
-    conversation_gpt.append({"role": "user", "content": message})
-
-def build_messages(conversation):
-    messages = []
-    for message in conversation:
-        messages.append({"role": message["role"], "content": message["content"]})
-    return messages
-
-
-def send_message_gpt(message):
-    global conversation_gpt
-    global total_tokens
-
-    if total_tokens > prmtoken:
-        print('\n\nWarning: reaching token limit. \nThis model maximum context length is ', prmtoken, ' in the messages,', prmtoken+maxtoken ,'in total.')
-        #print('\n Inizializing new conversation.')
-        #conversation_gpt.clear()
-        print('\n the first third of the conversation was forgotten')
-
-        quarter_length = len(conversation_gpt) // 3
-        conversation_gpt = conversation_gpt[quarter_length:]
-        if keep_persona:
-            conversation_gpt.append({"role": "system",
-                                     "content": dict['instructions']['persona']})
-
-    expand_conversation_gpt(message)
-    messages = build_messages(conversation_gpt)
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        max_tokens=maxtoken  # set max token
-    )
-    # Add the assistant's reply to the conversation
-    with open('conversation_log.txt', 'a', encoding='utf-8') as file:
-        file.write('---------------------------')
-        file.write('\nUser: '+str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))+'\n' + message)
-        if persona != '':
-            file.write('\n\n'+persona+':\n' + response.choices[0].message.content + '\n\n')
-        else:
-            file.write('\n\nGPT:\n' + response.choices[0].message.content + '\n\n')
-    conversation_gpt.append({"role": "assistant", "content": response.choices[0].message.content})
-
-    # Print the assistant's response
-    print_mess = message.replace('\r', '\n').replace('\n\n', '\n')
-    print('user:',print_mess,'\n...')
-    print(response.choices[0].message.content)
-
-    total_tokens = response.usage.total_tokens
-    print('prompt tokens:', total_tokens)
-
-
 #-----------------------------------------------------
 # Run script:
 while True:  # external cycle
@@ -246,6 +116,9 @@ while True:  # external cycle
         dict = dicteng
     else:
         dict = dictita
+
+    # choose model
+    op.choose_model()
     #----------
     #if language == '1':
     choose = ""
@@ -255,56 +128,48 @@ while True:  # external cycle
 
         # Verify user's choice
         if choose == '1':
-            with open('conversation_log.txt', 'a') as file:
+            with open('chat_log.txt', 'a') as file:
                 file.write('\n' + str(datetime.now()) + ": <You are asking the virtual assistant>\n")
             print("You chose option 1.")
 
         elif choose == '2':
             add = input(dict['instruct'])
-            with open('conversation_log.txt', 'a') as file:
+            with open('chat_log.txt', 'a') as file:
                 file.write('\n' + str(datetime.now()) + ": <You are asking the virtual assistant, system: "+add+"> \n")
             print("You chose option 2.")
 
         elif choose == '3':
-            persona = input('Tell me who you want to talk to:')
-            persona_dict = {'character': "You are now impersonating "+persona+". Please reflect "+persona+"'s traits in all interactions. Make sure to use an appropriate language style and uphold an attitude or mindset that aligns with "+persona+"'s character.",
-                            'personaggio': "Stai impersonando "+persona+", . Ricorda di riflettere i tratti di "+persona+" in tutte le interazioni. Assicurati di utilizzare uno stile linguistico appropriato e di mantenere un atteggiamento o una mentalità in linea con il personaggio di "+persona}
-            add = persona_dict['character']
-            with open('conversation_log.txt', 'a') as file:
-                file.write('\n' + str(datetime.now()) + ': <You asked to ' + persona + '>\n')
-            print("I'm connecting you with " + persona)
+            op.persona = input('Tell me who you want to talk to:')
+            with open('chat_log.txt', 'a') as file:
+                file.write('\n' + str(datetime.now()) + ': <You asked to ' + op.persona + '>\n')
+            print("\nI'm connecting you with " + op.persona)
 
         elif choose == '4':
             print("You chose to have some conversation.")
             assistant = input(dict['instruction'])
             if assistant == '1':
-                conversation_gpt.append({"role": "system",
+                op.chat_gpt.append({"role": "system",
                                          "content": dict['instructions']['assistant']})
-                with open('conversation_log.txt', 'a') as file:
+                with open('chat_log.txt', 'a') as file:
                     file.write('\n' + str(datetime.now()) + ': <You are chatting with the general assistant>\n')
             elif assistant == '2':
-                conversation_gpt.append({"role": "system",
+                op.chat_gpt.append({"role": "system",
                                          "content": dict['instructions']['poet']})
-                with open('conversation_log.txt', 'a') as file:
+                with open('chat_log.txt', 'a') as file:
                     file.write('\n' + str(datetime.now()) + ': <You are chatting with the greatest poet ever>\n')
             elif assistant == '3':
-                conversation_gpt.append({"role": "system",
+                op.chat_gpt.append({"role": "system",
                                          "content": dict['instructions']['scientist']})
-                with open('conversation_log.txt', 'a') as file:
+                with open('chat_log.txt', 'a') as file:
                     file.write(
                         '\n' + str(datetime.now()) + ': <You are chatting with the greatest  scientist ever>\n')
             elif assistant == '4':
-                persona = input(dict['instructions']['tell me'])
-                persona_dict = {'character': "You are now impersonating "+persona+". Please reflect "+persona+"'s traits in all interactions. Make sure to use an appropriate language style and uphold an attitude or mindset that aligns with "+persona+"'s character.",
-                                'personaggio': "Stai impersonando "+persona+", . Ricorda di riflettere i tratti di "+persona+" in tutte le interazioni. Assicurati di utilizzare uno stile linguistico appropriato e di mantenere un atteggiamento o una mentalità in linea con il personaggio di "+persona}
-                if language == '1':
-                    content = persona_dict['character']
-                elif language == '2':
-                    content = persona_dict['personaggio']
-                conversation_gpt.append({"role": "system",
-                                         "content": content})
-                with open('conversation_log.txt', 'a') as file:
-                    file.write('\n' + str(datetime.now()) + ': <You are chatting with ' + persona + '>\n')
+                #persona = input(dict['instructions']['tell me'])
+                op.persona = input('Tell me who you want to talk to:')
+
+                with open('chat_log.txt', 'a') as file:
+                    file.write('\n' + str(datetime.now()) + ': <You are chatting with ' + op.persona + '>\n')
+                print("\nI'm connecting you with " + op.persona)
         else:
             print("Invalid choice.")
 
@@ -335,10 +200,16 @@ while True:  # external cycle
             previous_content = clipboard_content  # Update previous content
 
             if choose == '4':
-                send_message_gpt(clipboard_content)
+                if language == 1:
+                    op.send_message(clipboard_content, persona=op.persona, language='eng')
+                else:
+                    op.send_message(clipboard_content, persona=op.persona, language='ita')
 
             else:
-                ask_gpt(clipboard_content)
+                if language == 1:
+                    op.ask_gpt(clipboard_content, persona=op.persona, language='eng')
+                else:
+                    op.ask_gpt(clipboard_content, persona=op.persona, language='ita')
 
         time.sleep(1)  # Wait 1 second before checking again
         if safe_word == 'restartnow':
@@ -351,8 +222,8 @@ while True:  # external cycle
             #global maxtoken
             #custom = get_boolean_input('\ndo you want to cutomize token options? (yes:1/no:0):')
             #if custom:
-            maxtoken = int(input('set max response tokens (800 default):'))
+            op.maxtoken = int(input('set max response tokens (800 default):'))
             break
 
-
 # openai.error.InvalidRequestError: This model's maximum context length is 4097 tokens.  However, you requested 4116 tokens (2116 in the messages, 2000 in the completion).  Please reduce the length of the messages or completion.
+#%%
