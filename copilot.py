3280e7fa04b2ce87fd22a7524475f42e833fb22a
johndef64 <116732521+johndef64@users.noreply.github.com>
2024-03-01 18:06:36 +0100

    Update copilot.py

--- copilot.py
+++ copilot.py
@@ -1,27 +1,16 @@
 #%%
 import os
 import sys
-
 import requests
-def get_gitfile(url, flag='', dir = os.getcwd()):
-    url = url.replace('blob','raw')
-    response = requests.get(url)
-    file_name = flag + url.rsplit('/',1)[1]
-    file_path = os.path.join(dir, file_name)
-    with open(file_path, 'wb') as file:
-        file.write(response.content)
 
-'''if not os.getcwd().endswith('pychatgpt.py'):
-    handle="https://raw.githubusercontent.com/johndef64/pychatgpt/main/"
-    files = ["pychatgpt.py"]
-    for file in files:
-       url = handle+file
-       get_gitfile(url)'''
+def get_gitfile(url, dir=os.getcwd()):
+    file = requests.get(url.replace('blob', 'raw')).content
+    open(os.path.join(dir, url.rsplit('/', 1)[1]), 'wb').write(file)
+    print('file downloaded')
+if not os.path.exists('pychatgpt.py'):
+    get_gitfile("https://raw.githubusercontent.com/johndef64/pychatgpt/main/pychatgpt.py")
 
-if 'google.colab' in sys.modules:
-    clip=False
-else:
-    clip=True
+clip = False if 'google.colab' in sys.modules else True
 
 import pychatgpt as op
 
@@ -35,20 +24,18 @@ file = op.load_file(path)
 
 #%% expand chat
 op.clearchat()
-#op.expand_chat('''  ''', 'system')
-op.expand_chat('''  ''', 'assistant')
-#op.expand_chat('''  ''', 'user')
-
+op.expand_chat(file, 'user') #system #assistant
 #%%
-m=''' '''
-op.chatgpt(m, 'gpt-4', 1000, clip)
-#%%
-op.clearchat()
-m='''
+m = ''' 
 
 '''
-#op.crick(m, clip=False)
-op.crick(m, 'gpt-4', 1000, clip)
+op.delamain(m, 'gpt-4', 1000, clip)
+#%%
+op.clearchat()
+m = ''' 
+
+'''
+op.leonardo(m, 'gpt-4', 1000, clip)
 #%%
 op.clearchat()
 m = ''' 
@@ -60,19 +47,23 @@ op.clearchat()
 m = ''' 
 
 '''
-op.leonardo(m, 'gpt-4', 1000, clip)
+op.crick(m, 'gpt-4', 1000, clip)
 #%%
 op.clearchat()
 m = '''
-oggi vado al mare. domani andrò al mare. ieri sono andato al mare '''
+Today I'm going to the sea. Tomorrow I will go to the sea. Yesterday I went to the sea. '''
 op.japanese_teacher(m, 'gpt-4', 1000, clip)
 #%%
+op.clearchat()
+m = ''' 
 
-m=''
+'''
 op.roger(m, 'gpt-4', 1000, clip)
 #%%
+op.clearchat()
+m = ''' 
 
-m=''' io bene, grazie. sono distratto e non riesco a lavorare'''
+'''
 op.bestie(m, 'gpt-4', 1000, clip)
 op.text2speech(op.reply,'onyx', play=True)
 #%%
@@ -80,11 +71,42 @@ op.text2speech(op.reply,'onyx', play=True)
 op.clearchat()
 op.talk_with('bestie', 8, 'onyx')
 #%% Load chat
+
 op.load_chat()
 df = op.pd.DataFrame(op.chat_gpt)
 print(df.head())
 #%%
-m=''' '''
+m='''
+ 
+'''
 op.chatgpt(m, 'gpt-4', 1000, clip)
 #%%
 op.chat_gpt
+#%%
+# Notes
+'''
+Translation: 今日、私は海に行きます。 明日、海に行くつもりです。昨日、海に行きました。
+
+Hiragana: きょう、わたしは うみ に いきます。 あした、うみに いくつもりです。きのう、うみに いきました。
+
+Romaji: Kyou, watashi wa umi ni ikimasu. Ashita, umi ni iku tsumori desu. Kinou, umi ni ikimashita.
+
+Analysis:
+   Sentence1:
+       Noun: 今日 (きょう) - kyou - today
+       Pronoun: 私 (わたし) - watashi - I
+       Particle: は (wa) - topic marking particle, often linking to the subject of the sentence.
+       Noun: 海 (うみ) - umi - sea
+       Particle: に (ni) - direction particle indicates the direction or goal of the action. 
+       Verb: 行きます (いきます) - ikimasu - go
+
+   Sentence2:
+       Noun: 明日 (あした) - ashita - tomorrow
+       Noun: 海 (うみ) - umi - sea
+       Verb: 行くつもりです (いくつもりです) - iku tsumori desu - plan to go 
+
+   Sentence3:
+       Noun: 昨日 (きのう) - kinou - yesterday
+       Noun: 海 (うみ) - umi - sea
+       Verb: 行きました (いきました) - ikimashita - went
+'''
