b5c8d1338123979ed8124c559f5fcd04b23d2eb5
johndef64 <116732521+johndef64@users.noreply.github.com>
2024-05-22 17:56:42 +0200

    commit repo refresh

---
+++ conda-copilot-lab.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Opening Anaconda Prompt and changing directory...
+
+REM Get the current working directory
+set "current_dir=%cd%"
+
+REM Replace "myenv" with your conda enviroment
+start cmd /k "call C:\Users\Utente\anaconda3\Scripts\activate.bat && cd /d %current_dir% && conda activate myenv && jupyter lab --NotebookApp.token='' --NotebookApp.password='' "%current_dir%\copilot_notebook.ipynb" "
