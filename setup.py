8cbe312f8a30991ce433f04ca37999415eca8631
johndef64 <116732521+johndef64@users.noreply.github.com>
2024-11-20 12:00:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mychatgpt',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),  # Automatically find packages in your project
     install_requires=[         # Optional: specify dependencies
         "openai", "tiktoken", "langdetect", "pandas", "pyperclip", "gdown","scipy", "nltk", "PyPDF2", 'cryptography', 'matplotlib', "pygame", "sounddevice", "soundfile", "keyboard", "pillow"
