0d7f0f115a6a938cc386bfb0da06b9e6a653769d
johndef64 <116732521+johndef64@users.noreply.github.com>
2024-12-11 17:46:33 +0100

    Update pygtp.R

--- mychatgpt/pygtp.R
+++ mychatgpt/pygtp.R
@@ -12,6 +12,7 @@ library(reticulate)
 # Set up a Python environment within R
 #use_python("C:/Users/Utente/anaconda3/envs/torchic2/python.exe")
 # Print the Python version and executable path being used by reticulate
+
 cat("Python version:", py_config()$version, "\n") # version
 cat("Python executable:", py_config()$python, "\n") # executable path
 
@@ -19,44 +20,24 @@ input <- function(prompt) {
   cat(prompt)  # Display the prompt to the user
   readLines(n = 1)  # Read a single line of input from the user
 }
+
 # Import the necessary Python module (pychatgpt.py)
-pychatgpt <- import("pychatgpt")
-pychatgpt$api_key <- pychatgpt$simple_decrypter(input('write your password'), pychatgpt$api_hash)
+mygpt <- import("mychatgpt")
+mygpt$api_key <- mygpt$simple_decrypter(input('write your password'), mygpt$api_hash)
 # Access the GPT class from the pychatgpt package
-op <- pychatgpt$GPT()
-R <- pychatgpt$R
-C <- pychatgpt$C
-R$chat('ciao')
+op <- mygpt$GPT()
+R  <- mygpt$R
+C  <- mygpt$C
+R$c('ciao')
 
-Rc <- function(m) {
-  R$chat(m)
-}
-Rc('@ciao')
+R$model <- 'gpt-4o-2024-08-06'
 
-Rp <- function(m) {
-  m <- paste0(m, op$pc$paste())
-  R$chat(m)
-}
+R$cp("correct thid code:\n")
 
 
 BIO <- function(m) {
-  pychatgpt$mendel(m, 'gpt-4o-2024-08-06')
+  mygpt$mendel(m, 'gpt-4o-2024-08-06')
 }
-
-BIOp <- function(m) {
-  m <- paste0(m, op$pc$paste())
-  BIO$chat(m)
-}
-
-Py <- function(m) {
-  pychatgpt$delamain$chat(m, 'gpt-4o-2024-08-06')
-}
-
-Pyp <- function(m) {
-  m <- paste0(m, op$pc$paste())
-  C$chat(m)
-}
-
 
 
 #USAGE
